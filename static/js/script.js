097bdece2b65f1e2347dbb59fcdda2fe7802cbf7
VictorLCM1227 <victor.luan.costa.matos@gmail.com>
2026-09-10 23:59:36 -0300

    v1 finalizada

--- static/js/script.js
+++ static/js/script.js
@@ -1,13 +1,16 @@
 
 let selecionado = document.querySelector('#selecionado')
 let horarioSelecionado = null
+let situacaoSelecionada = null
 
-let btnAgendar = document.querySelector('buttonAgendar')
+let btnAgendar = document.querySelector('#buttonAgendar')
+btnAgendar.addEventListener('click', agendar)
 
 let linhas = document.querySelectorAll('tr[id]')
 
 linhas.forEach(function(linha) {
     linha.addEventListener('click',clicar)
+
 })
 
 
@@ -15,12 +18,30 @@ function clicar(evento) {
     let linha = evento.currentTarget
 
     horarioSelecionado = linha.querySelectorAll('td')[0].innerText
+    situacaoSelecionada = linha.querySelectorAll('td')[1]
 
-    let situacao = linha.querySelectorAll('td')[1]
-    
-    if (situacao.innerText == 'Livre') {
 
-        selecionado.innerText = `Horário escolhido: ${horarioSelecionado}`
+    if (situacaoSelecionada.innerText == 'Livre') {
+
+        selecionado.innerHTML = `<p>Horário escolhido: ${horarioSelecionado}</p>`
+    } else {
+        selecionado.innerHTML = `<p>Horário escolhido: Já está ocupado</p>`
     }
 
+}
+
+// Evento 2 confirmar Agendamento
+
+
+function agendar() {
+    if (horarioSelecionado == null) {
+        alert('[ERRO]: Por favor selecione um horário para agendar consulta!')
+    } else {
+        if (situacaoSelecionada.innerText == 'Livre') {
+            situacaoSelecionada.innerText = 'Ocupado'
+            selecionado.innerHTML = '<p>Horário escolhido: </p>'
+        } else {
+            alert('[ERRO]: O horário escolhido já está ocupado!')
+        }
+    }
 }
